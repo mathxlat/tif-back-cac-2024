0ac4704aa739f213a8b8737fa761e57135d7575f
Mathías Ezequiel Latrónico <mathiezelat@gmail.com>
2024-06-30 19:31:15 -0300

    Modifica la clase Cartelera para que se pueda conectar y desconectar de la base de datos

--- Cartelera.py
+++ Cartelera.py
@@ -4,6 +4,11 @@ import mysql.connector.errorcode
 
 class Cartelera:
     def __init__(self, host, user, password, database):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.database = database
+
         self.db = mysql.connector.connect(
             host=host,
             user=user,
@@ -34,55 +39,90 @@ class Cartelera:
                                 poster VARCHAR(255) NOT NULL,
                                 trailer VARCHAR(255) NOT NULL)''')
         self.db.commit()
+        self.desconectar()
 
-        self.cursor.close()
+    def conectar(self):
+        self.db = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
         self.cursor = self.db.cursor(dictionary=True)
 
+    def desconectar(self):
+        self.cursor.close()
+        self.db.close()
+
     def obtener_peliculas(self):
-        self.cursor.execute('SELECT * FROM peliculas')
-        peliculas = self.cursor.fetchall()
+        self.conectar()
+        try:
+            self.cursor.execute('SELECT * FROM peliculas')
+            peliculas = self.cursor.fetchall()
+        finally:
+            self.desconectar()
         return peliculas
 
     def obtener_pelicula(self, id):
-        self.cursor.execute('SELECT * FROM peliculas WHERE id = %s', (id,))
-        return self.cursor.fetchone()
+        self.conectar()
+        try:
+            self.cursor.execute('SELECT * FROM peliculas WHERE id = %s', (id,))
+            pelicula = self.cursor.fetchone()
+        finally:
+            self.desconectar()
+        return pelicula
 
     def agregar_pelicula(self, titulo, sinopsis, fecha_de_estreno, actores, genero, director, pais, duracion, poster, trailer):
-        sql = '''INSERT INTO peliculas (titulo,
-                sinopsis,
-                fecha_de_estreno,
-                actores,
-                genero,
-                director,
-                pais,
-                duracion,
-                poster,
-                trailer) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-        values = (titulo, sinopsis, fecha_de_estreno, actores,
-                  genero, director, pais, duracion, poster, trailer)
-        self.cursor.execute(sql, values)
-        self.db.commit()
-        return self.cursor.lastrowid
+        self.conectar()
+        try:
+            sql = '''INSERT INTO peliculas (titulo,
+                    sinopsis,
+                    fecha_de_estreno,
+                    actores,
+                    genero,
+                    director,
+                    pais,
+                    duracion,
+                    poster,
+                    trailer) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+            values = (titulo, sinopsis, fecha_de_estreno, actores,
+                    genero, director, pais, duracion, poster, trailer)
+            self.cursor.execute(sql, values)
+            self.db.commit()
+            id = self.cursor.lastrowid
+        finally:
+            self.desconectar()
+        return id
 
     def modificar_pelicula(self, id, nuevo_titulo, nueva_sinopsis, nueva_fecha_de_estreno, nuevos_actores, nuevo_genero, nuevo_director, nuevo_pais, nueva_duracion, nuevo_poster, nuevo_trailer):
-        sql = '''UPDATE peliculas SET 
-                titulo = %s,
-                sinopsis = %s,
-                fecha_de_estreno = %s,
-                actores = %s,
-                genero = %s,
-                director = %s,
-                pais = %s,
-                duracion = %s,
-                poster = %s,
-                trailer = %s WHERE id = %s'''
-        valores = (nuevo_titulo, nueva_sinopsis, nueva_fecha_de_estreno, nuevos_actores,
-                   nuevo_genero, nuevo_director, nuevo_pais, nueva_duracion, nuevo_poster, nuevo_trailer, id)
-        self.cursor.execute(sql, valores)
-        self.db.commit()
-        return self.cursor.rowcount > 0
+        self.conectar()
+        try:
+            sql = '''UPDATE peliculas SET 
+                    titulo = %s,
+                    sinopsis = %s,
+                    fecha_de_estreno = %s,
+                    actores = %s,
+                    genero = %s,
+                    director = %s,
+                    pais = %s,
+                    duracion = %s,
+                    poster = %s,
+                    trailer = %s WHERE id = %s'''
+            valores = (nuevo_titulo, nueva_sinopsis, nueva_fecha_de_estreno, nuevos_actores,
+                    nuevo_genero, nuevo_director, nuevo_pais, nueva_duracion, nuevo_poster, nuevo_trailer, id)
+            self.cursor.execute(sql, valores)
+            self.db.commit()
+            es_modificado = self.cursor.rowcount > 0
+        finally:
+            self.desconectar()
+        return es_modificado
 
     def eliminar_pelicula(self, id):
-        self.cursor.execute('DELETE FROM peliculas WHERE id = %s', (id,))
-        self.db.commit()
-        return self.cursor.rowcount > 0
+        self.conectar()
+        try:
+            self.cursor.execute('DELETE FROM peliculas WHERE id = %s', (id,))
+            self.db.commit()
+            es_eliminado = self.cursor.rowcount > 0
+        finally:
+            self.desconectar()
+        return es_eliminado
