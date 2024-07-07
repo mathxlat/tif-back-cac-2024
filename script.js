34439c34f84dcc84138e06eb626697ed49d7d97b
paulaavilla <pmv_1998@hotmail.com>
2024-07-07 02:41:30 -0300

    agregar eliminar y modificar

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             .then(function (data) {
                 alert('Película agregada correctamente.');
+                mostrarPeliculas(URL);  //actualizar la tabla de peliculas con los datos actuales
             })
             
             .catch(function (error) {
@@ -41,9 +42,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.getElementById('trailer').value = "";
             });
     });
-    fetchAndDisplayMovie(URL);
-    function fetchAndDisplayMovie(URL){  
-        fetch(URL)
+    mostrarPeliculas(URL); //al cargar la pagina cargue la tabla por primera vez
+    
+}); 
+
+function borrarPelicula(id){
+    const URL = "https://mathxlat.pythonanywhere.com/peliculas";
+    fetch(URL + '/' + id, {
+        method: 'DELETE',
+    })
+    .then(function (response) {
+    if (response.ok) {
+            alert('Pelicula eliminada correctamente.');
+            mostrarPeliculas(URL);
+        } else {
+            throw new Error('Error al eliminar la película.');
+        }
+    })
+}
+
+function mostrarPeliculas(PAGINA){
+    let URL = PAGINA; //URL recibe "PAGINA" que es la base de datos cuando la llama  
+    fetch(URL)
             .then(function (response){
                 if (response.ok) {
                     return response.json();
@@ -51,11 +71,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     throw new Error('Error al obtener las peliculas.');
                 }
             })
-            .then (function (data){ 
-                let tablaPeliculas = document.getElementById('peliculasTable');
-                console.log(data)
-                for (let pelicula of data) {
-                    let fila = document.createElement('tr');
+        .then(function (data){
+            let tablaPeliculas = document.getElementById('peliculasTable');
+            tablaPeliculas.innerHTML = '';
+            for (let pelicula of data) {
+                let fila = document.createElement('tr');
                     fila.innerHTML= '<td>' + pelicula.id + '</td>' +
                         '<td>' + pelicula.titulo + '</td>' +
                         '<td align="right">' + pelicula.sinopsis + '</td>' +
@@ -66,18 +86,46 @@ document.addEventListener('DOMContentLoaded', () => {
                         '<td align="right">' + pelicula.pais + '</td>' +
                         '<td align="right">' + pelicula.duracion + '</td>' +
                         '<td><img src=' + URL + "/posters/" + pelicula.poster.split("/")[6] + ' alt="Poster" style="width: 100px;"></td>' +
-                        '<td align="right">' + pelicula.trailer + '</td>';
-
+                        '<td align="right">' + pelicula.trailer + '</td>' +
+                        '<td text-align="center"><button onclick="borrarPelicula(' + pelicula.id + ')">Borrar</button><button onclick="modificarPelicula(' + pelicula.id + ')">Modificar</button></td>';
                     tablaPeliculas.appendChild(fila);
-                }
-            })
-    }
-}); 
-   
+            }
+        })
+}
 
-
-
-
+function modificarPelicula(id){
+    const URL = "https://mathxlat.pythonanywhere.com/peliculas";
+    const formPelicula = document.getElementById("peliculaForm");
+    const formData = new FormData(formPelicula);
+    fetch(URL + '/' + id, {
+        method: 'PUT',
+        body: formData,
+    })
+    .then(function (response) {
+        if (response.ok) {
+            alert('Película modificada correctamente.');
+            mostrarPeliculas(URL);
+        } else {
+            alert("Complete todos los campos para modificar la pelicula.")
+            throw new Error('Error al modificar la película.'); //Error en consola
+            
+        }
+    })
+    //finalmente limpia todos los campos
+    .finally(function () {
+        document.getElementById('peliculaId').value = "";
+        document.getElementById('titulo').value = "";
+        document.getElementById('sinopsis').value = "";
+        document.getElementById('fecha_de_estreno').value = "";
+        document.getElementById('actores').value = "";
+        document.getElementById('genero').value = "";
+        document.getElementById('director').value = "";
+        document.getElementById('pais').value = "";
+        document.getElementById('duracion').value = "";
+        document.getElementById('poster').value = "";
+        document.getElementById('trailer').value = "";
+    });
+}
 
 
 
